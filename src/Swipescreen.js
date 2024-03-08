42edbc3fd147c1c181d06d5dcaa865494c92422b
Justin R Mealey <justinmealey16@gmail.com>
2024-03-07 17:46:10 -0800

    Add artists state, add artists dropdown
    json response sends frontend the tracks and artists, added a state to handle
    incoming data. Made a dropdown list of all artists, so that a user can click
    on one and filter their songs to be swiped on next.

--- src/Swipescreen.js
+++ src/Swipescreen.js
@@ -10,18 +10,20 @@ export default function Swipescreen({ token }) {
     const { state } = useLocation();
     const { playlist_id, name } = state;
     const [tracks, setTracks] = useState([null]);
+    const [artists, setArtists] = useState([null]);
     const [tracksToRemove, setTracksToRemove] = useState([]);
     const [loading, setLoading] = useState(true);
     const [trackCounter, setTrackCounter] = useState(0);
 
     useEffect(() => {
         async function getTracks(playlist_id) {
-            const response = await fetch('http://localhost:8000/tracks?' + new URLSearchParams({
+            const response = await fetch('http://localhost:8000/tracks_and_artists?' + new URLSearchParams({
                 playlist_id: playlist_id,
             }));
             const json = await response.json();
             console.log("Tracks: ", json);
-            setTracks(json);
+            setTracks(json.tracks);
+            setArtists(json.artists);
             setLoading(false);
         }
         getTracks(playlist_id);
@@ -37,7 +39,20 @@ export default function Swipescreen({ token }) {
             <div className="swipe-screen">
                 {<WebPlayback token={token} track_list={tracks.map(item => item.track)} playlist_name={name} />}
             </div>
+            <FilterArtistButton artists={artists}/>
         </div>
     );
 }
 
+//use tracks state to get artists instead of doing another fetch, make dropdown of all artists
+//so the user can select who to filter
+function FilterArtistButton({artists}){
+    if(artists[0]){
+        return (
+        <select>
+            {artists.map((artist) => <option>{artist.name}</option>)}
+        </select>
+    )
+    }
+    return <div></div>
+}
