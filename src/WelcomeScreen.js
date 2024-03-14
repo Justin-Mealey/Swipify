04c13ad2e6bd18f08a1e23ec8b54a131d49e27ba
Christian Solano <christiansolano@Christians-MacBook-Air-2.local>
2024-03-14 00:27:27 -0700

    Remove Drake loading screen

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,39 +1,48 @@
-import React from 'react';
-import { useState, useEffect} from 'react'
-import Playlist from './Playlist.js'
-import TopBar from './TopBar.js'; // Adjust the import path based on your file structure
+import React, { useState, useEffect } from 'react';
+import Playlist from './Playlist.js';
+import TopBar from './TopBar.js';
+import Spinner from './Spinner.js';
 
-
-export default function WelcomeScreen(){
-    const [playlists, setPlaylists] = useState([{name: "Loading...", id : ""}]);
+export default function WelcomeScreen() {
+    const [playlists, setPlaylists] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-
         async function getPlaylists() {
-            const response = await fetch('http://localhost:8000/playlists');
-            console.log(response);
-            const json = await response.json();
-            setPlaylists(json);
+            try {
+                const response = await fetch('http://localhost:8000/playlists');
+                const json = await response.json();
+                setPlaylists(json);
+            } catch (error) {
+                console.error('Error fetching playlists:', error);
+            } finally {
+                setIsLoading(false);
+            }
         }
+        getPlaylists();
+    }, []);
 
-    getPlaylists();
+    let listItems = null;
+    if (playlists) {
+        listItems = playlists.map((playlist) => (
+            <li key={playlist.id}>
+                <Playlist name={playlist.name} id={playlist.id} images={playlist.images} />
+            </li>
+        ));
+    }
 
-
-  }, []);
-
-    let listItems = playlists.map((playlist) => 
-        <li key={playlist.id}>
-            <Playlist name={playlist.name} id={playlist.id} images={playlist.images}/>
-        </li>
-    )
+    if (isLoading) {
+        return <Spinner />;
+    }
 
     return (
         <>
-            <TopBar/>
+            <TopBar />
             <div className="subheading">SELECT. SWIPE. SAVE.</div>
             <ul className='playlist-grid'>
                 {listItems}
             </ul>
         </>
-    )
+    );
 }
+
